68c0e49b8e027bbe507cf0d1e19959f1accb5751
Snizhana <snizhanapetrushka@gmail.com>
2026-07-03 11:40:06 +0300

    fix: refactor catalog to use Suspense for build error

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,71 +1,12 @@
 "use client";
-import { useSearchParams, useRouter } from "next/navigation";
-import { useCampers } from "@/hooks/useCampers";
-import CamperList from "@/components/CamperList/CamperList";
-import FilterSidebar from "@/components/FilterSidebar/FilterSidebar";
-import styles from "./catalog.module.css";
+import { Suspense } from "react";
+import CatalogContent from "@/components/CatalogContent/CatalogContent";
 import Loader from "@/components/Loader/Loader";
-import EmptyState from "@/components/NotFound/NotFount";
 
 export default function CatalogPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const VALID_FORMS = [
-    "alcove",
-    "panel_van",
-    "integrated",
-    "semi_integrated",
-  ] as const;
-  const VALID_TRANSMISSIONS = ["automatic", "manual"] as const;
-  const VALID_ENGINES = ["diesel", "petrol", "hybrid", "electric"] as const;
-
-  const getValidValue = <T extends string>(
-    value: string | null,
-    validValues: readonly T[],
-  ): T | undefined => {
-    return validValues.includes(value as T) ? (value as T) : undefined;
-  };
-  const filters = {
-    location: searchParams.get("location") || undefined,
-    form: getValidValue(searchParams.get("form"), VALID_FORMS),
-    transmission: getValidValue(
-      searchParams.get("transmission"),
-      VALID_TRANSMISSIONS,
-    ),
-    engine: getValidValue(searchParams.get("engine"), VALID_ENGINES),
-  };
-
-  const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useCampers(filters);
-
-  const campers = data?.pages.flatMap((p) => p.campers) || [];
-  const isEmpty = status === "success" && campers.length === 0;
-
-  const handleClearFilters = () => {
-    router.push("/catalog");
-  };
-
   return (
-    <div className={styles.container}>
-      {status === "pending" && <Loader />}
-
-      <aside className={styles.sidebar}>
-        <FilterSidebar />
-      </aside>
-
-      <section className={styles.list}>
-        {isEmpty ? (
-          <EmptyState onClearFilters={handleClearFilters} />
-        ) : (
-          <CamperList
-            campers={data?.pages.flatMap((p) => p.campers) || []}
-            hasNextPage={!!hasNextPage}
-            fetchNextPage={fetchNextPage}
-            isFetchingNextPage={isFetchingNextPage}
-          />
-        )}
-      </section>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <CatalogContent />
+    </Suspense>
   );
 }
