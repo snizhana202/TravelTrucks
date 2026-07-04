6c479569054bb05c85a89ad539608b029a919f2d
Snizhana <snizhanapetrushka@gmail.com>
2026-07-04 15:24:42 +0300

    feat: add CamperCard component with styles

--- src/components/CamperCard/CamperCard.tsx
+++ src/components/CamperCard/CamperCard.tsx
@@ -2,19 +2,12 @@ import Image from "next/image";
 import { Camper } from "@/types/camper";
 import styles from "./CamperCard.module.css";
 import { iconMap } from "@/constants/icons";
+import Link from "next/link";
+import CamperFeatures from "@/components/CamperFeatures/CamperFeatures";
 
 export default function CamperCard({ camper }: { camper: Camper }) {
   const StarIcon = iconMap.star;
   const MapIcon = iconMap.map;
-
-  const features = [
-    {
-      icon: iconMap[camper.transmission.toLowerCase()],
-      label: camper.transmission,
-    },
-    { icon: iconMap[camper.engine.toLowerCase()], label: camper.engine },
-    { icon: iconMap[camper.form.toLowerCase()], label: camper.form },
-  ];
 
   return (
     <div className={styles.card}>
@@ -27,36 +20,37 @@ export default function CamperCard({ camper }: { camper: Camper }) {
       />
 
       <div className={styles.content}>
-        <div className={styles.camperHeader}>
-          <h3>{camper.name}</h3>
-          <p>€{camper.price.toFixed(2)}</p>
-        </div>
-
-        <div className={styles.info}>
-          <div className={styles.rating}>
-            {StarIcon && <StarIcon className={styles.icon} color="#FFC531" />}
-            {camper.rating} ({camper.totalReviews} Reviews)
+        <div className={styles.camperContent}>
+          <div className={styles.camperHeader}>
+            <h3>{camper.name}</h3>
+            <p>€{camper.price.toFixed(2)}</p>
           </div>
-          <div className={styles.location}>
-            {MapIcon && <MapIcon className={styles.icon} />}
-            {camper.location}
+
+          <div className={styles.info}>
+            <div className={styles.rating}>
+              {StarIcon && <StarIcon className={styles.icon} color="#ffc531" />}
+              <span>
+                {camper.rating} ({camper.totalReviews} Reviews)
+              </span>
+            </div>
+            <div className={styles.location}>
+              {MapIcon && <MapIcon className={styles.icon} />}
+              <span>{camper.location}</span>
+            </div>
           </div>
         </div>
 
         <p className={styles.description}>{camper.description}</p>
 
-        <div className={styles.tags}>
-          {features.map((item, i) => (
-            <span key={i} className={styles.tag}>
-              {item.icon && typeof item.icon === "function" && (
-                <item.icon className={styles.icon} />
-              )}
-              {item.label.replace("_", " ")}
-            </span>
-          ))}
-        </div>
+        <CamperFeatures camper={camper} limit={3} />
 
-        <button className={styles.button}>Show more</button>
+        <Link
+          href={`/catalog/${camper.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.button}>Show more</button>
+        </Link>
       </div>
     </div>
   );
